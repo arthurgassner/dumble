3bbaddbf8c00ee0749ba5c1644ce83b538af27cf
Arthur Gassner <arth.gassner@gmail.com>
2022-02-19 12:07:02 +0100

    Run prettier

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,8 +10,8 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the letter in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the letter.
+        Guess the letter in 6 tries. After each guess, the color of the tiles
+        will change to show how close your guess was to the letter.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -37,12 +37,10 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <h1 className="text-l ml-2.5 grow font-medium dark:text-white">
           "I don't get it"
         </h1>
-        <p className="text-sm italic">
-        You, 2022
-        </p>
+        <p className="text-sm italic">You, 2022</p>
         <p className="mt-3 text-sm text-gray-500 dark:text-gray-300">
-        This is a joke on the popular game <i>Wordle</i>. 
-        It's essentially a 1-letter version of the game.
+          This is a joke on the popular game <i>Wordle</i>. It's essentially a
+          1-letter version of the game.
         </p>
       </div>
 
@@ -50,14 +48,11 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <h1 className="text-l ml-2.5 grow font-medium dark:text-white">
           "But it's dumb?"
         </h1>
-        <p className="text-sm italic">
-        You, 2022
-        </p>
+        <p className="text-sm italic">You, 2022</p>
         <p className="mt-3 text-sm text-gray-500 dark:text-gray-300">
-        Yeah haha
+          Yeah haha
         </p>
       </div>
-
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
         This is an open source joke on the word-guessing game we all know and
