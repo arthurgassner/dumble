9b23b8f69e4401120a25a1694d36a649d3950a81
Arthur Gassner <arth.gassner@gmail.com>
2022-02-19 11:18:51 +0100

    Add an explanation as to what this is

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -33,6 +33,32 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         The letter U is not the correct letter.
       </p>
 
+      <div className="mt-10">
+        <h1 className="text-l ml-2.5 grow font-medium dark:text-white">
+          "I don't get it"
+        </h1>
+        <p className="text-sm italic">
+        You, 2022
+        </p>
+        <p className="mt-3 text-sm text-gray-500 dark:text-gray-300">
+        This is a joke on the popular game <i>Wordle</i>. 
+        It's essentially a 1-letter version of the game.
+        </p>
+      </div>
+
+      <div className="mt-10">
+        <h1 className="text-l ml-2.5 grow font-medium dark:text-white">
+          "But it's dumb?"
+        </h1>
+        <p className="text-sm italic">
+        You, 2022
+        </p>
+        <p className="mt-3 text-sm text-gray-500 dark:text-gray-300">
+        Yeah haha
+        </p>
+      </div>
+
+
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
         This is an open source joke on the word-guessing game we all know and
         love -{' '}
